c3c42de4d32600ed3a866f96cf4dd32fd8494c99
salmanpary <salmanpary@gmail.com>
2024-02-11 15:34:37 +0530

    lisa intro

---
+++ src/app/privacy/4/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import styles from './privacy.module.css'
+const Page = () => {
+  return (
+    <div className={styles.dashboard}>page</div>
+  )
+}
+
+export default Page
